e201e3da4ad473c079baacc6c5a4c60a71792855
skips <wieichdashasse@gmail.com>
2018-10-11 14:51:24 +0100

    Added Base Testing Framework
    based on InterfaceManager tests

--- src/MainEntry.cpp
+++ src/MainEntry.cpp
@@ -1,5 +1,7 @@
 #include <Windows.h>
 
+// Inject injection handler here trigger page fault initialize us. unlink us. return execution
+
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID) {
 	switch (fdwReason)
 	{
