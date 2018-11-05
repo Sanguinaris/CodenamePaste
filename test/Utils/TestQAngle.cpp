e877d29bf3d1ebad130fc2a05a8feeba42e66825
skips <wieichdashasse@gmail.com>
2018-11-05 23:31:57 +0000

    [CNP-16] Write Beginning of MathHelpa

--- test/Utils/TestQAngle.cpp
+++ test/Utils/TestQAngle.cpp
@@ -1,29 +1,93 @@
 #include <catch.hpp>
-/*
-class QAngle {
-public:
-        enum {
-                PITCH = 0,
-                YAW,
-                ROLL
-        };
+#include "Utils/QAngle.h"
 
-        float x=0.f, y=0.f, z=0.f;
+using namespace CodeNamePaste;
+using namespace Utils;
 
-        float operator[](int idx) const;
+float* GimmeAVec() {
+  float* fl = new float[3];
+  fl[0] = 1.f;
+  fl[1] = 1.f;
+  fl[2] = 1.f;
+  return fl;
+}
 
-        float& operator[](int idx);
+TEST_CASE("Can Access Parameters") {
+  const Vector vec{};
+  const Vector vec2{1.f, 2.f, 3.f};
 
-        bool operator==(const Vector& otherVec) const;
+  CHECK(vec[0] == 0.f);
+  CHECK(vec[1] == 0.f);
+  CHECK(vec[2] == 0.f);
+  CHECK(vec2[0] == 1.f);
+  CHECK(vec2[1] == 2.f);
+  CHECK(vec2[2] == 3.f);
+}
 
-        bool operator!=(const Vector& otherVec) const;
+TEST_CASE("Can Access Parameters and modify em") {
+  Vector vec{};
 
-        Vector& operator+=(const Vector& otherVec);
+  CHECK(vec[0] == 0.f);
+  CHECK(vec[1] == 0.f);
+  CHECK(vec[2] == 0.f);
+  vec[0] = 1.f;
+  vec[1] = 2.f;
+  vec[2] = 3.f;
+  CHECK(vec[0] == 1.f);
+  CHECK(vec[1] == 2.f);
+  CHECK(vec[2] == 3.f);
+}
 
-        Vector& operator-=(const Vector& otherVec);
+TEST_CASE("Can Access Parameters and modify em via x y and z") {
+  Vector vec{};
 
-        Vector operator-(const Vector& otherVec) const;
+  CHECK(vec.x == 0.f);
+  CHECK(vec.y == 0.f);
+  CHECK(vec.z == 0.f);
+  vec.x.get() = 1.f;
+  vec.y.get() = 2.f;
+  vec.z.get() = 3.f;
+  CHECK(vec.x == 1.f);
+  CHECK(vec.y == 2.f);
+  CHECK(vec.z == 3.f);
+}
 
-        Vector operator+(const Vector& otherVec) const;
-};
-*/
+TEST_CASE("Vector can compare itself") {
+  Vector vec{};
+  Vector otherVec{1.f};
+  CHECK(vec == vec);
+  CHECK_FALSE(vec == otherVec);
+  CHECK_FALSE(vec != vec);
+  CHECK(vec != otherVec);
+}
+
+TEST_CASE("Vector can add stuff") {
+  Vector vec{};
+  Vector vec2{1.f, 2.f, 3.f};
+
+  vec += vec2;
+  CHECK(vec == vec2);
+  vec += vec2;
+  CHECK((vec.x == 2.f && vec.y == 4.f && vec.z == 6.f));
+  vec = vec + vec;
+  CHECK((vec.x == 4.f && vec.y == 8.f && vec.z == 12.f));
+}
+
+TEST_CASE("Vector can subtract stuff") {
+  Vector vec{4.f, 8.f, 12.f};
+  Vector vec2{2.f, 4.f, 6.f};
+
+  CHECK((vec.x == 4.f && vec.y == 8.f && vec.z == 12.f));
+  vec -= vec2;
+  CHECK(vec == vec2);
+  vec = vec - vec;
+  CHECK(vec == Vector{});
+}
+
+TEST_CASE("Vector works with External Vector") {
+  Vector myVec = GimmeAVec();
+
+  CHECK(myVec.x == Approx(1.f));
+  CHECK(myVec.y == Approx(1.f));
+  CHECK(myVec.z == Approx(1.f));
+}
