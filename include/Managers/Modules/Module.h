e52181b80c2a97ffd55dadabd8bcb188e2e1fdce
Sanguinaris <wieichdashasse@gmail.com>
2018-11-14 16:09:25 +0000

    [CNP-XXX] Write Base Module Manager

---
+++ include/Managers/Modules/Module.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "IModule.h"
+
+namespace CodeNamePaste::Managers::Modules {
+	class Module : public IModule
+	{
+	public:
+		Module(std::string&& name);
+
+	public:
+		void DoInit() override;
+		bool DoShutdown() override;
+
+	public:
+		void Toggle() override;
+		void SetState(bool state) override;
+
+	public:
+		const bool IsActive() const override;
+		const std::string& GetModuleName() const override;
+
+	protected:
+		bool moduleState = false;
+
+	private:
+		const std::string moduleName;
+	};
+}
